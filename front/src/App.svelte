1aee665b81c2af563608160a2f9f7baf272379df
CoderLotl <j.e.delmar.87@gmail.com>
2023-07-26 19:46:22 -0300

    first commit

---
+++ front/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import frogLogo from './assets/frog.gif'  
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={frogLogo} class="logo frog" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.frog:hover
+  {
+    filter: drop-shadow(0 0 20px #2caa05);
+  }
+</style>
---
+++ front/src/lib/Counter.svelte
@@ -0,0 +1,40 @@
+<script>
+  let count = 0;
+  let message = '';
+
+  const increment = async () =>
+  {
+    count += 1;    
+  };
+
+  // Fetch data from index.php and update the 'message' variable
+  const fetchData = async () =>
+  {
+    try {
+      const response = await fetch('http://localhost'); // Assuming index.php is in the same domain
+      const data = await response.text();
+      if (response.status === 200 && data.trim() !== '') {
+        message = data;
+      } else {
+        message = 'Error fetching data'; // Display an error message if the request fails or the data is empty
+      }
+    } catch (error) {
+      message = `EXCEPTION: ${error}`; // Display an error message if an exception occurs
+    }
+  };
+  
+</script>
+
+<button on:click={increment}>
+  Press the button to test Svelte.
+</button>
+<br>
+<p>  
+  count is {count}
+</p>
+<br>
+<button on:click={fetchData}>
+  Press the button to test the server.
+</button>
+<br>
+<p>{message}</p>
